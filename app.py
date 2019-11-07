6ece6e5c0c69c6325f49a7c8ea3c3212eeabb322
Juliana Wuan <julianawuan@gmail.com>
2019-11-07 11:17:55 +0800

    Added store form. WIP flash message

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import peeweedbevolve
-from flask import Flask, render_template, request
-from models import db
+from flask import Flask, flash, render_template, request, redirect, url_for
+from models import db, Store
 
 app = Flask(__name__)
+.secret_key = b"\xe3\x86\xc7\xe7\xf2\xfc'\xb0c\xda)\xfa\xfe%\x13\x8b"
 
 
 @app.before_request
@@ -24,6 +25,25 @@ def migrate():
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/store', methods=['GET'])
+def store():
+    return render_template('store.html')
+
+
+@app.route("/store", methods=['POST'])
+def create_new_store():
+    new_store = Store(name=request.form.get('store_name'))
+    if new_store.save():
+        flash('Successfully saved!')
+        return redirect(url_for('store'))
+    else:
+        return render_template('store.html', name=request.form.get('store_name'))
+
+@app.route('/warehouse', methods=['GET'])
+def warehouse():
+    return render_template('warehouse.html')
 
 
 if __name__ == '__main__':
